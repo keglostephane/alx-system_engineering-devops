d90060fe21fafd8b74df5dc0b4fbb6e7c2c99a27
Dodzi Keglo <stephanekeglo@gmail.com>
2024-01-20 00:26:44 +0000

    Fix #4

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,8 +3,12 @@
 # package_version: 2.1.0
 # package_manager: pip3
 
+package { 'python3-pip':
+  ensure  => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  name     => flask,
   provider => 'pip3',
-  }
+  require  => Package['python3-pip'],
+}
