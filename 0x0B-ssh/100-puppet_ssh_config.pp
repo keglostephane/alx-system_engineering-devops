379535fb465a5b55d66c0d7561e203fdaca36345
Dodzi Keglo <stephanekeglo@gmail.com>
2024-01-22 05:51:45 +0000

    Fix #2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,8 @@
 # set up client SSH config file to connect to a server without password
 
 $str = "Host 18.207.234.107
-   User ubuntu
    IdentityFile ~/.ssh/school
-   PubkeyAuthentication yes
+   PasswordAuthentication no
 "
 file { 'config':
   ensure  => file,
