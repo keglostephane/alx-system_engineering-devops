032e77156ad19dc2b3ff255a94f97d87464db229
Dodzi Keglo <stephanekeglo@gmail.com>
2024-03-06 04:44:09 +0000

    Add 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""reddit_subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """return the number of subscribers for a given subreddit.
+
+    :param subredit: subreddit name
+    :type subredit: str
+    :return: the number of subscribers for the subreddit
+    :rtype: int
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Firefox/5.0'}
+    req = requests.get(url, headers=headers, allow_redirects=False)
+
+    if req.status_code == 200:
+        result = req.json()
+        return result.get("data").get("subscribers")
+    else:
+        return 0
